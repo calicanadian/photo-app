8a7a0ec9fc04201817e7fd13416e547802599d76
Your name <Your email>
2019-06-13 09:36:08 -0700

    Added sendgrid configuration variables. Updated gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,20 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load heroku vars from local file
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
